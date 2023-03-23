fcdcf72359da00321e9d6ee9282737525a0df124
Explisto <119876723+Explisto@users.noreply.github.com>
2023-03-23 23:23:06 +0300

    Update 27. Remove Element.py

--- ДЗ_4/27. Remove Element.py
+++ ДЗ_4/27. Remove Element.py
@@ -4,18 +4,18 @@ class Solution
         p_1 = 0
         p_2 = len(nums)-1
 
-        while p_1 = p_2
+        while p_1 <= p_2:
 
-            while nums[p_2] == val
+            while nums[p_2] == val:
                 p_2 -= 1
                 
-                if p_2 == -1
+                if p_2 == -1:
                     return 0
             
-            if p_1  p_2
+            if p_1 > p_2:
                 return p_1
             
-            if nums[p_1] == val
+            if nums[p_1] == val:
                 nums[p_1] = nums[p_2]
                 nums[p_2] = val
                 p_2 -= 1
