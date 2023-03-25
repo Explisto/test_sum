262cf93a14bcc05691f5d67aa20bbb4cde67e729
Explisto <119876723+Explisto@users.noreply.github.com>
2023-03-25 22:15:41 +0300

    Update 234. Palindrome Linked List.py

--- ДЗ_4/234. Palindrome Linked List.py
+++ ДЗ_4/234. Palindrome Linked List.py
@@ -6,13 +6,40 @@
 #https://leetcode.com/problems/palindrome-linked-list/
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        current = head
-        s_pol = ""
-        while (current):
-            s_pol = s_pol + str(current.val)
-            current = current.next
         
-        if (s_pol == s_pol[::-1]):
+        current_slow = head
+        current_fast = head
+
+        if head == None or head.next == None:
             return True
-        else:
-            return False
+        
+        while current_fast and current_fast.next:
+            
+            current_slow = current_slow.next
+
+            if current_fast.next:
+                current_fast = current_fast.next.next
+        
+        current_1 = head
+        current_2 = current_slow
+        next_p = None
+        prev = None
+
+        while current_2:
+            next_p = current_2.next
+            current_2.next = prev
+            prev = current_2
+            current_2 = next_p
+        
+        current_1 = head
+        current_2 = prev
+
+        while current_1 and current_2:
+            
+            if current_1.val != current_2.val:
+                return False
+            
+            current_1 = current_1.next
+            current_2 = current_2.next
+        
+        return True
