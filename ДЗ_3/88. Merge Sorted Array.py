dd78ebe4c23d8972ba77a9ae8893859cae53c4fd
Explisto <119876723+Explisto@users.noreply.github.com>
2023-03-26 11:31:29 +0300

    Update 88. Merge Sorted Array.py

--- ДЗ_3/88. Merge Sorted Array.py
+++ ДЗ_3/88. Merge Sorted Array.py
@@ -5,20 +5,17 @@ class Solution:
         p1 = m - 1
         p2 = n - 1
         p3 = m + n - 1
-        sw = 0
-        flag = True
 
-        while p1 >= 0 and p2 >= 0:
+        while p2 >= 0:
             
-            if nums1[p1] > nums2[p2]:
+            if p1 >=0 and nums1[p1] > nums2[p2]:
                 nums1[p3] = nums1[p1]
                 p1 -= 1
             
             else:
                 nums1[p3] = nums2[p2]
                 p2 -= 1
+            
             p3 -= 1
-
-        nums1[:p2 + 1] = nums2[:p2 + 1]
 
         return nums1
