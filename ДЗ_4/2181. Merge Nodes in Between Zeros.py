b711a64cb6ae450c923015b629eabcf0f43ccd4d
Explisto <119876723+Explisto@users.noreply.github.com>
2023-03-26 10:12:01 +0300

    Update 2181. Merge Nodes in Between Zeros.py

--- ДЗ_4/2181. Merge Nodes in Between Zeros.py
+++ ДЗ_4/2181. Merge Nodes in Between Zeros.py
@@ -6,32 +6,21 @@
 #https://leetcode.com/problems/merge-nodes-in-between-zeros/
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        current_1 = head
+
+        current_1 = head.next
         current_2 = head
         rez = 0
-        sw = 0
-        ls = []
 
         while current_1:
-            
+
             while current_1.val != 0:
-                rez = rez + current_1.val
+                rez += current_1.val
                 current_1 = current_1.next
             
+            current_2.next = current_1
+            current_2.next.val = rez
             current_1 = current_1.next
-            
-            if rez != 0:
-                ls.append(rez)
-                rez = 0
+            current_2 = current_2.next
+            rez = 0
         
-        while sw < len(ls):
-            current_2.val = ls[sw]
-            if sw == len(ls) - 1:
-                current_2.next = None
-            else:
-                current_2 = current_2.next
-            sw += 1
-    
-
-        return head
+        return head.next
